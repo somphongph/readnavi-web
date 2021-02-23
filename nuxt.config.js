2bbec6fb747f5aa2ef60a76bba532e386f733706
Somphong Phuangmalai <somphong.ph@gmail.com>
2021-02-23 14:50:33 +0700

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,6 @@ import colors from 'vuetify/es5/util/colors'
 
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
-
-  target: 'server',
   head: {
     titleTemplate: '%s - readnavi',
     title: 'readnavi',
